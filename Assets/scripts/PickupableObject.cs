2bccf0731652977780fc6d3b3643d21800d0ed2b
mark zaki <120737045+Markzaki55@users.noreply.github.com>
2023-04-23 20:53:11 +0200

    fixed the pickup....kinda

--- Assets/scripts/PickupableObject.cs
+++ Assets/scripts/PickupableObject.cs
@@ -1,3 +1,57 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class PickupableObject : MonoBehaviour, IPickupable
+// {
+//     private Rigidbody rb;
+//     private Vector3 originalPosition;
+//     private bool isHeld = false;
+//     private Transform holder;
+//     public float dropOffset = 1.0f;
+//     public LayerMask pickupLayer;
+
+//     void Start()
+//     {
+//         rb = GetComponent<Rigidbody>();
+//         originalPosition = transform.position;
+//     }
+
+//     public void OnPickup()
+//     {
+//         rb.isKinematic = true;
+//         isHeld = true;
+//         holder = Camera.main.transform  ;
+//     }
+
+//     public void OnDrop()
+//     {
+//         rb.isKinematic = false;
+//         rb.velocity = holder.forward * 5f; 
+//         transform.position = holder.position + holder.forward * dropOffset;
+//         transform.rotation = holder.rotation;
+//         isHeld = false;
+//         holder = null;
+//     }
+
+//     void Update()
+//     {
+//         if (isHeld)
+//         {
+//             transform.position = Camera.main.transform.position + Camera.main.transform.forward * 0.8f;
+//             transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
+//         }
+//     }
+
+//     void OnCollisionEnter(Collision collision)
+//     {
+//         if (!isHeld && pickupLayer == (pickupLayer | (1 << collision.gameObject.layer)))
+//         {
+//             originalPosition = transform.position;
+//         }
+//     }
+// }
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +63,7 @@ public class PickupableObject : MonoBehaviour, IPickupable
     private bool isHeld = false;
     private Transform holder;
     public float dropOffset = 1.0f;
-    public LayerMask pickupLayer;
+    public float dropForce = 4.0f;
 
     void Start()
     {
@@ -19,15 +73,16 @@ public class PickupableObject : MonoBehaviour, IPickupable
 
     public void OnPickup()
     {
+        
         rb.isKinematic = true;
         isHeld = true;
-        holder = Camera.main.transform  ;
+        holder = Camera.main.transform;
     }
 
     public void OnDrop()
     {
         rb.isKinematic = false;
-        rb.velocity = holder.forward * 5f; 
+        rb.AddForce(holder.forward * dropForce, ForceMode.Impulse);
         transform.position = holder.position + holder.forward * dropOffset;
         transform.rotation = holder.rotation;
         isHeld = false;
@@ -43,11 +98,11 @@ public class PickupableObject : MonoBehaviour, IPickupable
         }
     }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        if (!isHeld && pickupLayer == (pickupLayer | (1 << collision.gameObject.layer)))
-        {
-            originalPosition = transform.position;
-        }
-    }
+    // void OnCollisionEnter(Collision collision)
+    // {
+    //     if (!isHeld)
+    //     {
+    //         originalPosition = transform.position;
+    //     }
+    // }
 }
--- Assets/scripts/Playerpicksystem.cs
+++ Assets/scripts/Playerpicksystem.cs
@@ -1,3 +1,76 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using TMPro;
+
+// public class Playerpicksystem : MonoBehaviour
+// {
+//     [SerializeField] private float pickupDistance = 2f;
+//     [SerializeField] private LayerMask pickupLayer;
+//     [SerializeField] private TextMeshProUGUI pickText; 
+//     private IPickupable heldObject;
+//     private RaycastHit hit;
+
+//     private void Update()
+//     {
+//         HandlePickup();
+//         HandlePickupText();
+//     }
+
+//     private void HandlePickup()
+//     {
+//         if (Input.GetKeyDown(KeyCode.Q))
+//         {
+//             if (heldObject == null)
+//             {
+//                 if (Physics.Raycast(transform.position, transform.forward, out hit, pickupDistance, pickupLayer))
+//                 {
+//                     heldObject = hit.collider.gameObject.GetComponent<IPickupable>();
+//                     if (heldObject != null)
+//                     {
+//                         heldObject.OnPickup();
+//                     }
+//                 }
+//             }
+//             else
+//             {
+//                 heldObject.OnDrop();
+//                 heldObject = null;
+//             }
+//         }
+//     }
+
+//     private void HandlePickupText()
+//     {
+//         if (heldObject == null)
+//         {
+//             if (Physics.Raycast(transform.position, transform.forward, out hit, pickupDistance, pickupLayer))
+//             {
+//                 IPickupable pickupable = hit.collider.gameObject.GetComponent<IPickupable>();
+//                 if (pickupable != null)
+//                 {
+//                     pickText.gameObject.SetActive(true);
+//                     pickText.text = "Press Q to pick up";
+//                 }
+//             }
+//             else
+//             {
+//                 pickText.gameObject.SetActive(false);
+//             }
+//         }
+//         else
+//         {
+//             pickText.gameObject.SetActive(true);
+//             pickText.text = "Press Q to drop";
+//         }
+//     }
+
+//     private void OnDrawGizmosSelected()
+//     {
+//         Gizmos.color = Color.yellow;
+//         Gizmos.DrawRay(transform.position, transform.forward * pickupDistance);
+//     }
+// }
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,9 +80,10 @@ public class Playerpicksystem : MonoBehaviour
 {
     [SerializeField] private float pickupDistance = 2f;
     [SerializeField] private LayerMask pickupLayer;
+    [SerializeField] private Vector3 raycastSize = new Vector3(0.2f, 0.2f, 2.0f);
     [SerializeField] private TextMeshProUGUI pickText; 
     private IPickupable heldObject;
-    private RaycastHit hit;
+    private Collider[] hits;
 
     private void Update()
     {
@@ -23,9 +97,10 @@ public class Playerpicksystem : MonoBehaviour
         {
             if (heldObject == null)
             {
-                if (Physics.Raycast(transform.position, transform.forward, out hit, pickupDistance, pickupLayer))
+                hits = Physics.OverlapBox(transform.position + transform.forward * pickupDistance / 2f, raycastSize / 2f, transform.rotation, pickupLayer);
+                if (hits.Length > 0)
                 {
-                    heldObject = hit.collider.gameObject.GetComponent<IPickupable>();
+                    heldObject = hits[0].gameObject.GetComponent<IPickupable>();
                     if (heldObject != null)
                     {
                         heldObject.OnPickup();
@@ -42,24 +117,34 @@ public class Playerpicksystem : MonoBehaviour
 
     private void HandlePickupText()
     {
-        if (Physics.Raycast(transform.position, transform.forward, out hit, pickupDistance, pickupLayer))
+        if (heldObject == null)
         {
-            IPickupable pickupable = hit.collider.gameObject.GetComponent<IPickupable>();
-            if (pickupable != null && heldObject == null)
+            hits = Physics.OverlapBox(transform.position + transform.forward * pickupDistance / 2f, raycastSize / 2f, transform.rotation, pickupLayer);
+            if (hits.Length > 0)
             {
-                pickText.gameObject.SetActive(true);
-                pickText.text = "Press Q to pick up";
+                IPickupable pickupable = hits[0].gameObject.GetComponent<IPickupable>();
+                if (pickupable != null)
+                {
+                    pickText.gameObject.SetActive(true);
+                    pickText.text = "Press Q to pick up";
+                }
+            }
+            else
+            {
+                pickText.gameObject.SetActive(false);
             }
         }
         else
         {
-            pickText.gameObject.SetActive(false);
+            pickText.gameObject.SetActive(true);
+            pickText.text = "Press Q to drop";
         }
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawRay(transform.position, transform.forward * pickupDistance);
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawWireCube(Vector3.forward * pickupDistance / 2f, raycastSize);
     }
 }
